3f9c55ad3e178028dba072b501566eb3b30f2745
Timures <bisemsk@gmail.com>
2019-11-05 18:49:35 +0600

    add gulpfile, package.json - compile sass, pug

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var cleanCSS = require('gulp-clean-css')
+var minify = require('gulp-minify')
+var pug = require('gulp-pug')
+var rename = require('gulp-rename')
+var browserSync = require('browser-sync').create()
+
+gulp.task('sass', function() {
+	return gulp.src('resources/sass/main.scss', 'resources/sass/**/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('dist/css'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('minify-css', () => {
+	return gulp.src('dist/css/*.css')
+	  .pipe(cleanCSS({compatibility: 'ie8'}))
+	  .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('compress', async function() {
+  	return gulp.src('resources/js/*.js')
+    .pipe(minify())
+    .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('pug-compile', function buildHTML() {
+    return gulp.src('./resources/templates/index.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(rename('./index.html'))
+        .pipe(gulp.dest('./'))
+});
+
+gulp.task('watch', function () {
+	gulp.watch('resources/templates/**/*.pug', gulp.series('pug-compile'));
+	gulp.watch('resources/sass/**/*.scss', gulp.series('sass'));
+	gulp.watch('resources/js/*.js', gulp.series('compress'));
+ });
+
+gulp.task('serve', gulp.series('sass', function() {
+
+	browserSync.init({
+		server: './'
+	});
+
+	gulp.watch('resources/sass/**/*.scss', gulp.series('sass'));
+	gulp.watch('*.html').on('change', browserSync.reload);
+}));
+
+gulp.task('default', gulp.series('serve'));
