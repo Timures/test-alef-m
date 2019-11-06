6cdcbd670aa773f579da4ea8b1c85adbce75c534
Timures <bisemsk@gmail.com>
2019-11-07 00:29:17 +0600

    add load data first time when site open, add color to btn in footer

--- dist/js/app.js
+++ dist/js/app.js
@@ -20081,6 +20081,25 @@
 	var jsParse = jQuery.parseJSON(jsObject); // console.log(jsParse);
 
 	$(document).ready(function () {
+	  // show on first time
+	  $('.first-content').html('');
+	  console.log('click f t');
+	  var firstHtmlElement = '';
+	  var thirdHtmlElement = '';
+	  $(jsParse).each(function (index, item) {
+	    // first 
+	    $(item.firstBlock).each(function (index_fb, item_fb) {
+	      firstHtmlElement += '<div class="first-content__item"><div class="first-content__logo"></div><div class="first-content__text">' + item_fb + '</div></div>';
+	    });
+	    $('.first-content').append(firstHtmlElement); // second
+
+	    $('.second-content').html(item.secondBlock); // third
+
+	    $(item.thirdBlock).each(function (index_tb, item_tb) {
+	      thirdHtmlElement += '<div class="third-content__item">' + item_tb + '</div>';
+	    });
+	    $('.third-content__item').html(thirdHtmlElement);
+	  });
 	  $('#sidebarCollapse').on('click', function () {
 	    $('#sidebar').toggleClass('collapsed');
 	  }); // read file
@@ -20093,7 +20112,7 @@
 	    $(jsParse).each(function (index, item) {
 	      // first 
 	      $(item.firstBlock).each(function (index_fb, item_fb) {
-	        firstHtmlElement += '<div class="first-content__item">' + item_fb + '</div>';
+	        firstHtmlElement += '<div class="first-content__item"><div class="first-content__logo"></div><div class="first-content__text">' + item_fb + '</div></div>';
 	      });
 	      $('.first-content').append(firstHtmlElement); // second
 
